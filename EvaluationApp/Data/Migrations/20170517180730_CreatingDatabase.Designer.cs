8b5b586a8fccf114a4557ebd66d1296d13ff5e6b
devalexer <alliemckay.dev@gmail.com>
2017-05-17 14:12:42 -0400

    Database Connected

--- EvaluationApp/Data/Migrations/20170517180730_CreatingDatabase.Designer.cs
+++ EvaluationApp/Data/Migrations/20170517180730_CreatingDatabase.Designer.cs
@@ -8,9 +8,10 @@ using EvaluationApp.Data;
 namespace EvaluationApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170517180730_CreatingDatabase")]
+    partial class CreatingDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
