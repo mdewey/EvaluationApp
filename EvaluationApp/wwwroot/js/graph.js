2b86b8c68fe1b58d5ef2730c8a1549f8dc849333
devalexer <alliemckay.dev@gmail.com>
2017-05-30 10:21:01 -0400

    Chart for DofU set up but not displaying

---
+++ EvaluationApp/wwwroot/js/graph.js
@@ -0,0 +1,27 @@
+﻿google.charts.load('current', { packages: ['corechart', 'line'] });
+google.charts.setOnLoadCallback(drawLineColors);
+
+function drawLineColors() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'StudentData');
+
+
+    data.addRows([
+        [0, 0], [1, 10], [2, 23], [3, 17], [4, 18], [5, 9],
+        [6, 30], [7, 40], [8, 42], [9, 47], [10, 44], [11, 48],
+    ]);
+
+    var options = {
+        hAxis: {
+            title: 'Time'
+        },
+        vAxis: {
+            title: 'LevelofUnderstanding'
+        },
+        colors: ['#a52714', '#097138']
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+};
