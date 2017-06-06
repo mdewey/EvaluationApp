c5f4eda9ed70e859d804aa36f591a001724a65f7
Mark Dewey <mark.t.dewey@gmail.com>
2017-06-05 22:10:37 -0400

    added gitignore

--- EvaluationApp/obj/Release/netcoreapp1.1/PubTmp/Out/wwwroot/js/studentSignUp.js
+++
@@ -1,9 +0,0 @@
-﻿// Submit form with id function.
-function submit_by_id() {
-    var name = document.getElementById("name").value;
-    if (validation()) // Calling validation function
-    {
-        document.getElementById("form_id").submit(); //form submission
-        alert(" Name : " + name + " \n Form Id : " + document.getElementById("form_id").getAttribute("id") + "\n\n Form Submitted Successfully......");
-    }
-}
